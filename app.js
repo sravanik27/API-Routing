4ed8eef522de7e6114e1877e6726d0d0c97f19a0
sravani <sravanikatyala@gmail.com>
2022-09-07 13:11:18 +0000

    API Routing

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Home Page");
+});
+
+module.exports = app;
+
+const express = require("express");
+const app = express();
+
+app.get("/about", (request, response) => {
+  response.send("About Page");
+});
+
+module.exports = app;
